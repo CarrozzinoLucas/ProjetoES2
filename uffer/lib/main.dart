c0b2888a1e6e94ddecd4dc2e16561aec61801613
alexsander <alexsandersoarest@gmail.com>
2024-04-10 19:48:32 -0300

    fix: firebase options is no longer null

--- uffer/lib/main.dart
+++ uffer/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uffer/widget_tree.dart';
+import 'firebase_options.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
