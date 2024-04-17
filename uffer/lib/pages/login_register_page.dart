c63419bc5ae52d76ef40c90e89e46d314e54a037
Giovana Beltrame <grbeltrame@gmail.com>
2024-04-17 20:00:59 -0300

    login page refacotred

--- uffer/lib/pages/login_register_page.dart
+++ uffer/lib/pages/login_register_page.dart
@@ -1,106 +1,45 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:uffer/pages/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart'; // Importe flutter_svg
 
-
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
-  @override 
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  String? errorMessage = '';
-  bool isLogin = true;
-
-  final TextEditingController _controllerEmail = TextEditingController();
-  final TextEditingController _controllerPassword = TextEditingController();
-
-  Future<void> signInWithEmailAndPassword() async {
-    try {
-      await Auth().signInWithEmailAndPassword(
-        email: _controllerEmail.text,
-        password: _controllerPassword.text
-      );
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
-      });
-    }
-  }
-
-  Future<void> createUserWithEmailAndPassword() async {
-    try {
-      await Auth().createUserWithEmailAndPassword(
-        email: _controllerEmail.text,
-        password: _controllerPassword.text,
-      );
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
-      });
-    }
-  }
-
-  Widget _title() {
-    return const Text('Firebase Auth');
-  }
-
-  Widget _entryField(
-    String title,
-    TextEditingController controller,
-  ) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: title,
-      ),
-    );
-  }
-
-  Widget _errorMessage() {
-    return Text(errorMessage == '' ? '' : '$errorMessage');
-  }
-
-  Widget _submitButton() {
-    return ElevatedButton(
-      onPressed:
-        isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
-      child: Text(isLogin ? 'Login' : 'Register'),
-    );
-  }
-
-  Widget _loginOrRegisterButton() {
-    return TextButton(
-      onPressed: () {
-        setState(() {
-          isLogin = !isLogin;
-        });
-      }, 
-      child: Text(isLogin ? 'Register instead' : 'Login instead'),
-      );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: _title(),
-      ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _entryField('email', _controllerEmail),
-            _entryField('password', _controllerPassword),
-            _errorMessage(),
-            _submitButton(),
-            _loginOrRegisterButton(),
+            SvgPicture.asset(
+              'images/logo-uffer.svg', // Caminho para o arquivo SVG
+              width: 150.0, // Defina uma largura adequada
+              height: 150.0, // Defina uma altura adequada
+            ),
+            SizedBox(height: 50), // Espaçamento entre o logo e o botão
+            OutlinedButton.icon(
+              icon: Image.asset(
+                'images/google-logo.png', // Caminho para o arquivo PNG do logo do Google
+                width: 24.0, // Largura da imagem
+                height: 24.0, // Altura da imagem
+                // Cor da imagem, se necessário
+              ),
+              label: Text(
+                'Entrar com Google',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w500, // médio
+                  fontSize: 16.0, // tamanho da fonte
+                ),
+              ),
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                minimumSize: Size(200, 50),
+              ),
+            ),
           ],
         ),
       ),
