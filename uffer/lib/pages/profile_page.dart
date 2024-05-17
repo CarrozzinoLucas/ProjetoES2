6db32320e65446894c302031b17af51a6413f431
Julia Camara <juliacamara@ibm.com>
2024-05-16 21:27:16 -0300

    feat(tela_perfil_passageiro[#55])

--- uffer/lib/pages/profile_page.dart
+++ uffer/lib/pages/profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:uffer/pages/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:uffer/widgets/common/action_painel.dart';
 import 'package:uffer/widgets/common/bottom_navigation_bar.dart';
+import 'package:uffer/widgets/common/rounded_square_button.dart';
 
 void main() {
   runApp(const ProfilePage());
@@ -26,7 +28,37 @@ class ProfilePage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [_signOutButton()],
+        children: [
+          Row(
+            children: [
+              RoundedSquareButton(innerLabel: Icon(Icons.list), onPressed: (){}),
+              RoundedSquareButton(innerLabel: Icon(Icons.thumb_up_alt_outlined), onPressed: (){}),
+              RoundedSquareButton(innerLabel: Icon(Icons.directions_car_filled_outlined), onPressed: (){}),
+              ActionPainel(
+                options: const [
+                  Option(
+                      label: 'Icaraí - UFF Praia Vermelha',
+                      sublabel: '8:30h, 1 passageiro',
+                      leftIconData: Icons.directions_car_filled_outlined,
+                      rightIconData: Icons.arrow_forward_ios),
+                  Option(
+                      label: 'São Francisco - UFF Valonguinho',
+                      sublabel: '10:30h, 2 passageiros',
+                      leftIconData: Icons.directions_car_filled_outlined,
+                      rightIconData: Icons.arrow_forward_ios),
+                  Option(
+                      label: 'Camboinhas - UFF Gragoatá',
+                      sublabel: '6:30h, 1 passageiro',
+                      leftIconData: Icons.directions_car_filled_outlined,
+                      rightIconData: Icons.arrow_forward_ios),
+                ],
+                onPressed: () {
+                  print('Painel pressionado!');
+                },
+              ),
+            ],
+          ),
+          _signOutButton()],
       ),
       bottomNavigationBar: const CustomBottomNavigationBar(),
     );
