ef25d8410654404617cdb16affc423832191bc86
alexsander <alexsandersoarest@gmail.com>
2024-05-20 15:37:37 -0300

    fix: home no longer overflows on smaller screens

--- uffer/lib/pages/home_page.dart
+++ uffer/lib/pages/home_page.dart
@@ -23,8 +23,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
         children: [
           const TopBarWidget(),
           Padding(
