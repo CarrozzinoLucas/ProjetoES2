1dc6342ad3a4e879a20dea045654096ca347d2e0
Julia Camara <juliacamara@ibm.com>
2024-05-20 15:17:05 -0300

    feat[#55]

--- uffer/lib/pages/profile_page.dart
+++ uffer/lib/pages/profile_page.dart
@@ -1,64 +1,107 @@
-import 'package:uffer/pages/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:uffer/pages/auth.dart';
 import 'package:uffer/widgets/common/action_painel.dart';
 import 'package:uffer/widgets/common/bottom_navigation_bar.dart';
 import 'package:uffer/widgets/common/rounded_square_button.dart';
+import 'package:uffer/widgets/home/top_bar.dart';
 
 void main() {
   runApp(const ProfilePage());
-}
-
-Widget _signOutButton() {
-  return const ElevatedButton(
-    onPressed: signOut,
-    child: Text('Sign Out'),
-  );
 }
 
 Future<void> signOut() async {
   await Auth().signOut();
 }
 
+Widget _signOutButton(BuildContext context) {
+  return ActionPainel(
+    options: const [Option(label: 'Sign Out', rightIconData: Icons.close)],
+    onPressed: () async {
+      await signOut();
+      Navigator.pop(context);
+    },
+  );
+}
+
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            children: [
-              RoundedSquareButton(innerLabel: Icon(Icons.list), onPressed: (){}),
-              RoundedSquareButton(innerLabel: Icon(Icons.thumb_up_alt_outlined), onPressed: (){}),
-              RoundedSquareButton(innerLabel: Icon(Icons.directions_car_filled_outlined), onPressed: (){}),
-              ActionPainel(
-                options: const [
-                  Option(
-                      label: 'Icaraí - UFF Praia Vermelha',
-                      sublabel: '8:30h, 1 passageiro',
+          const TopBarWidget(),
+          Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(children: [
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  RoundedSquareButton(
+                      iconData: Icons.assignment_outlined,
+                      outerLabel: 'Atividade',
+                      onPressed: () {}),
+                  const SizedBox(width: 16),
+                  RoundedSquareButton(
+                      iconData: Icons.thumb_up_alt_outlined,
+                      outerLabel: 'Em aberto',
+                      onPressed: () {}),
+                  const SizedBox(width: 16),
+                  RoundedSquareButton(
+                      iconData: Icons.directions_car_filled_outlined,
+                      outerLabel: 'Em aberto',
+                      onPressed: () {}),
+                ]),
+                const SizedBox(height: 32),
+                ActionPainel(
+                  options: [
+                    Option(
+                      label: 'Meus dados',
+                      leftIconData: Icons.person_outline,
+                      rightIconData: Icons.arrow_forward_ios,
+                      onPressed: () {
+                        print('Option 1 selected');
+                      },
+                    ),
+                    Option(
+                      label: 'Meus carros',
                       leftIconData: Icons.directions_car_filled_outlined,
-                      rightIconData: Icons.arrow_forward_ios),
-                  Option(
-                      label: 'São Francisco - UFF Valonguinho',
-                      sublabel: '10:30h, 2 passageiros',
-                      leftIconData: Icons.directions_car_filled_outlined,
-                      rightIconData: Icons.arrow_forward_ios),
-                  Option(
-                      label: 'Camboinhas - UFF Gragoatá',
-                      sublabel: '6:30h, 1 passageiro',
-                      leftIconData: Icons.directions_car_filled_outlined,
-                      rightIconData: Icons.arrow_forward_ios),
-                ],
-                onPressed: () {
-                  print('Painel pressionado!');
-                },
-              ),
-            ],
-          ),
-          _signOutButton()],
+                      rightIconData: Icons.arrow_forward_ios,
+                      onPressed: () {
+                        print('Option 2 selected');
+                      },
+                    ),
+                    Option(
+                      label: 'Locais salvos',
+                      leftIconData: Icons.star_border,
+                      rightIconData: Icons.arrow_forward_ios,
+                      onPressed: () {
+                        print('Option 3 selected');
+                      },
+                    ),
+                    Option(
+                      label: 'Configurações',
+                      leftIconData: Icons.settings_outlined,
+                      rightIconData: Icons.arrow_forward_ios,
+                      onPressed: () {
+                        print('Option 3 selected');
+                      },
+                    ),
+                    Option(
+                      label: 'Entrar em contato conosco',
+                      leftIconData: Icons.chat_bubble_outline_outlined,
+                      rightIconData: Icons.arrow_forward_ios,
+                      onPressed: () {
+                        print('Option 3 selected');
+                      },
+                    ),
+                  ],
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 16),
+                _signOutButton(context),
+              ]))
+        ],
       ),
       bottomNavigationBar: const CustomBottomNavigationBar(),
     );
