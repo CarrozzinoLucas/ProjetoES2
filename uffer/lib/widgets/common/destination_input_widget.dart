10ac563d8fa7bb1656d8f2526156923912f01c4c
alexsander <alexsandersoarest@gmail.com>
2024-05-20 20:50:55 -0300

    refactor: removed redundant sizedBox

--- uffer/lib/widgets/common/destination_input_widget.dart
+++ uffer/lib/widgets/common/destination_input_widget.dart
@@ -6,15 +6,15 @@ class DestinationInput extends StatelessWidget {
   final double iconSize;
   final void Function()? onTap;
   final void Function(String)? onSubmitted;
-  final FocusNode? focusNode; 
-  
+  final FocusNode? focusNode;
+
   const DestinationInput({
     required this.label,
     this.prefixIconData,
     this.iconSize = 16.0,
     this.onTap,
     this.onSubmitted,
-    this.focusNode, 
+    this.focusNode,
     Key? key,
   }) : super(key: key);
 
@@ -22,37 +22,35 @@ class DestinationInput extends StatelessWidget {
   Widget build(BuildContext context) {
     TextEditingController controller_ = TextEditingController();
 
-    return SizedBox(
-      width: 328, 
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100.0),
-          border: Border.all(color: const Color(0xFFE7E8EE)),
-          color: const Color(0xFFF9FAFB),
-        ),
-        child: TextField(
-          controller: controller_,
-          focusNode: focusNode, // Definindo o focusNode
-          onSubmitted: onSubmitted,
-          onTap: onTap,
-          decoration: InputDecoration(
-            contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
-            labelText: label,
-            hintText: '',
-            border: InputBorder.none,
-            prefixIcon: prefixIconData != null
-                ? Icon(
-                    prefixIconData,
-                    size: iconSize,
-                    color: const Color(0XFF79747E),
-                  )
-                : null,
-            prefixIconConstraints: const BoxConstraints(minWidth: 64),
-            // floatingLabelBehavior: FloatingLabelBehavior.always,
-            labelStyle: const TextStyle(
-                // height: 2,
-                ),
-          ),
+    return Container(
+      width: 250,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        border: Border.all(color: const Color(0xFFE7E8EE)),
+        color: const Color(0xFFF9FAFB),
+      ),
+      child: TextField(
+        controller: controller_,
+        focusNode: focusNode, // Definindo o focusNode
+        onSubmitted: onSubmitted,
+        onTap: onTap,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
+          labelText: label,
+          hintText: '',
+          border: InputBorder.none,
+          prefixIcon: prefixIconData != null
+              ? Icon(
+                  prefixIconData,
+                  size: iconSize,
+                  color: const Color(0XFF79747E),
+                )
+              : null,
+          prefixIconConstraints: const BoxConstraints(minWidth: 64),
+          // floatingLabelBehavior: FloatingLabelBehavior.always,
+          labelStyle: const TextStyle(
+              // height: 2,
+              ),
         ),
       ),
     );
