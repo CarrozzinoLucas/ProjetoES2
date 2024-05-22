a8166a89454aa9fd62bc160b2acf9422254570e2
alexsander <alexsandersoarest@gmail.com>
2024-05-22 11:27:12 -0300

    fix: rounded rect btn no longer overflows

--- uffer/lib/widgets/common/rounded_rectangle_button.dart
+++ uffer/lib/widgets/common/rounded_rectangle_button.dart
@@ -22,44 +22,43 @@ class RoundedRectangleButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPressed,
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        SizedBox(
-            width: width,
-            height: height,
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color(0xFFF9FAFB),
-                borderRadius: BorderRadius.circular(borderRadius),
-                border: Border.all(color: Color(0xFFE7E8EE)),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFF9FAFB),
+          borderRadius: BorderRadius.circular(borderRadius),
+          border: Border.all(color: const Color(0xFFE7E8EE)),
+        ),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 8, bottom: 8, left: 16, right: 20),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFE7EEFF),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(child: icon),
+                ),
               ),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: Center(
-                      child: Container(
-                        width: 40, // Largura do círculo
-                        height: 40, // Altura do círculo
-                        decoration: const BoxDecoration(
-                          color: Color(0xFFE7EEFF), // Cor do círculo
-                          shape: BoxShape.circle, // Forma do círculo
-                        ),
-                        child: Center(child: icon),
-                      ),
+              if (label != null)
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    label!,
+                    style: const TextStyle(
+                      color: Color(0XFF44474E),
                     ),
                   ),
-                  if (label != null) const SizedBox(width: 8),
-                  if (label != null)
-                    Text(
-                      label!,
-                      style: const TextStyle(
-                        color: Color(0XFF44474E),
-                      ),
-                    ),
-                ],
-              ),
-            ))
-      ]),
+                ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
