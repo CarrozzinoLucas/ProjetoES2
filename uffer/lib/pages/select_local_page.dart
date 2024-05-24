b23fe57236e696b5d0f9efe8b66e54c826a2249a
floatLuciano <lucianoandrade@id.uff.br>
2024-05-24 11:48:39 -0300

    ajustes #71

--- uffer/lib/pages/select_local_page.dart
+++ uffer/lib/pages/select_local_page.dart
@@ -3,6 +3,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uffer/pages/confirm_local_page.dart';
 import 'package:google_maps_webservice/places.dart';
 import 'package:uffer/widgets/common/action_painel.dart';
+import 'package:uffer/pages/list_saved_places_page.dart';
 import 'package:uffer/widgets/common/autocomplete_destination_input_widget.dart';
 import 'package:uffer/widgets/common/map_widget.dart';
 import 'package:uffer/widgets/common/top_back_button.dart';
@@ -118,6 +119,31 @@ class _SelectLocalPageState extends State<SelectLocalPage> {
                       },
                     ),
                   ],
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                ActionPainel(
+                  options: const [
+                    Option(
+                        label: 'Locais salvos',
+                        leftIconData: Icons.star_border,
+                        rightIconData: Icons.arrow_forward_ios),
+                    Option(
+                        label: 'Selecionar no mapa',
+                        leftIconData: Icons.place_outlined,
+                        rightIconData: Icons.arrow_forward_ios),
+                  ],
+                  onPressed: () => {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            const ListSavedPlacesPage(),
+                        transitionDuration: Duration.zero,
+                      ),
+                    )
+                  },
                 ),
                 const SizedBox(
                   height: 24,
