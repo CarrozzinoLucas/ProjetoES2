45c5609e767c93da441b97b95313ae2655a3efcd
floatLuciano <lucianoandrade@id.uff.br>
2024-05-27 23:04:54 -0300

    Colocar pins interativos no mapa #69

--- uffer/lib/pages/confirm_local_page.dart
+++ uffer/lib/pages/confirm_local_page.dart
@@ -1,35 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:uffer/pages/driver_on_way_page.dart';
-import 'package:uffer/pages/ride_not_found_page.dart';
 import 'package:uffer/widgets/common/blue_button.dart';
 import 'package:uffer/widgets/common/destination_input_widget.dart';
 import 'package:uffer/widgets/common/map_widget.dart';
 import 'package:uffer/widgets/common/top_back_button.dart';
 import 'package:uffer/widgets/common/draggable_widget.dart';
+import 'package:uffer/pages/driver_on_way_page.dart';
 
-class ConfirmLocalPage extends StatelessWidget {
+class ConfirmLocalPage extends StatefulWidget {
   final LatLng location;
-  final String address; 
+  final String address;
 
   const ConfirmLocalPage({
     Key? key,
     required this.location,
-    required this.address, 
+    required this.address,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final initialCameraPosition = CameraPosition(
-      target: location,
-      zoom: 20.0,
+  _ConfirmLocalPageState createState() => _ConfirmLocalPageState();
+}
+
+class _ConfirmLocalPageState extends State<ConfirmLocalPage> {
+  late LatLng _location;
+  late String _address;
+  final Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _location = widget.location;
+    _address = widget.address;
+    _addMarker(_location, _address);
+  }
+
+  void _addMarker(LatLng position, String address) {
+    final MarkerId markerId = MarkerId(address);
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: position,
+      infoWindow: InfoWindow(title: address),
+      onTap: () {
+  
+      },
     );
-    TextEditingController _addressController = TextEditingController(text: address);
+
+    setState(() {
+      _markers.add(marker);
+    });
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _addressController = TextEditingController(text: _address);
+
+    final CameraPosition initialCameraPosition = CameraPosition(
+      target: _location,
+      zoom: 19.0,
+    );
 
     return Scaffold(
       extendBodyBehindAppBar: true,
       body: Stack(children: [
-        MapWidget(initialCameraPosition: initialCameraPosition),
+        MapWidget(
+          initialCameraPosition: initialCameraPosition,
+          markers: _markers,
+          //onMarkerTap: _onMarkerTap,
+        ),
         const TopBackButton(),
         DraggableWidget(
           initialChildSize: 0.3,
